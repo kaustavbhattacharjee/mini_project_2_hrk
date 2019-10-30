3367f2b49f3387829ce6bc8723c227d6e5ba26e4
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-10-30 00:15:38 -0400

    task: updated project skeleton 2

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,4 +1,18 @@
 from Calculator.def_files.Confidence_interval import Confidence_interval
+from Calculator.def_files.Mean import Mean
+from Calculator.def_files.Median import Median
+from Calculator.def_files.Mode import Mode
+from Calculator.def_files.P_value import P_value
+from Calculator.def_files.Pop_correlation_coefficient import Pop_correlation_coefficient
+from Calculator.def_files.Proportion import Proportion
+from Calculator.def_files.Sample_mean import Sample_mean
+from Calculator.def_files.Sample_sd import Sample_sd
+from Calculator.def_files.Sd import Sd
+from Calculator.def_files.Standardised_score import Standardised_score
+from Calculator.def_files.Variance import Variance
+from Calculator.def_files.Variance_popu_proportion import Variance_popu_proportion
+from Calculator.def_files.Variance_sample_proportion import Variance_sample_proportion
+from Calculator.def_files.Z_score import Z_score
 
 class Calculator:
     def __init__(self):
@@ -7,30 +21,44 @@ class Calculator:
         #return Mean()
         pass
     def median(self):
+        # return Median()
         pass
     def mode(self):
+        # return Mode()
         pass
     def sd(self):
+        # return Sd()
         pass
     def variance_popu_proportion(self):
+        # return Variance_popu_proportion()
         pass
     def z_score(self):
+        # return Z_score()
         pass
     def standardised_score(self):
+        # return Standardised_score()
         pass
     def pop_correlation_coefficient(self):
+        # return Pop_correlation_coefficient()
         pass
     def confidence_interval(self):
+        # return Confidence_interval()
         pass
     def variance(self):
+        # return Variance()
         pass
     def p_value(self):
+        # return P_value()
         pass
     def proportion(self):
+        # return Proportion()
         pass
     def sample_mean(self):
+        # return Sample_mean()
         pass
     def sample_sd(self):
+        # return Sample_sd()
         pass
     def variance_sample_proportion(self):
+        # return Variance_sample_proportion()
         pass
