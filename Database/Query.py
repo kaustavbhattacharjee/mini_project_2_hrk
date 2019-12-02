e5b94a2346b94bf73eafa1edb1ce8ca4e7073b27
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-12-01 19:40:06 -0500

    feature: added order_by() method for Querying the Data

--- Database/Query.py
+++ Database/Query.py
@@ -146,3 +146,10 @@ print("~~Printing all customers but limiting to 2 and offsetting to 2:~~")
 for row in result:
    print ("Name: ",row.first_name," ",row.last_name, " Address:",row.address, " Email:",row.email)
 print("===========================")
+
+print("=========order_by()=========")
+result =  session.query(Create.Item).filter(Create.Item.name.like("wa%")).order_by(desc(Create.Item.cost_price)).all()
+print("~~Printing all items that start with wa and then it is sorted by the cost price in descending order:~~")
+for row in result:
+   print ("Name: ",row.name," Cost Price:",row.cost_price, " Selling Price:",row.selling_price, " Quantity:",row.quantity)
+print("===========================")
